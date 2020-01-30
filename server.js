84ec24bc76f2cf4967cc5f7a096f50f4bbe14ed0
Christopher Barnes <cbarnes7404@yahoo.com>
2020-01-29 23:12:16 -0800

    Users can see individual posts

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(expressValidator());
 
 // Routes
 app.get('/', (req, res) => {
-    res.render('posts-index', posts)
+    res.render('posts-index')
 });
 
 app.get('/posts/new', (req, res) => {
