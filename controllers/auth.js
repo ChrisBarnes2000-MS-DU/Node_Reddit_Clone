5840407c0532b7492beeb2a1cb47414afbf1e8f7
Christopher Barnes <cbarnes7404@yahoo.com>
2020-02-12 23:52:52 -0800

    users can logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,13 +6,14 @@ module.exports = app => {
     app.get("/sign-up", (req, res) => {
         res.render("sign-up");
     });
+
     // SIGN UP POST
     app.post("/sign-up", (req, res) => {
         // Create User and JWT
         const user = new User(req.body);
 
         user.save().then((user) => {
-            var token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: "60 days" });
+            const token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: "60 days" });
             res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
             res.redirect('/');
             })
@@ -20,5 +21,11 @@ module.exports = app => {
                 console.log(err.message);
                 return res.status(400).send({ err: err });
             });
+    });
+
+    // LOGOUT
+    app.get('/logout', (req, res) => {
+        res.clearCookie('nToken');
+        res.redirect('/');
     });
 };
