4aa654d69ac7b3cecb23df8ebf7a4d1c3f4bf673
Christopher Barnes <cbarnes7404@yahoo.com>
2020-02-26 00:56:07 -0800

    implement nested comments has issues with getting page

--- controllers/replies.js
+++ controllers/replies.js
@@ -10,7 +10,7 @@ module.exports = app => {
             .lean()
             .then(p => {
                 post = p;
-                return Comment.findById(req.params.commentId);
+                return Comment.findById(req.params.commentId).lean();
             })
             .then(comment => {
                 res.render("replies-new", { post, comment });
@@ -22,6 +22,32 @@ module.exports = app => {
 
     // CREATE REPLY
     app.post("/posts/:postId/comments/:commentId/replies", (req, res) => {
-        console.log(req.body);
+        // TURN REPLY INTO A COMMENT OBJECT
+        const reply = new Comment(req.body);
+        reply.author = req.user._id
+        // LOOKUP THE PARENT POST
+        Post.findById(req.params.postId)
+            .lean()
+            .then(post => {
+                // FIND THE CHILD COMMENT
+                Promise.all([
+                    reply.save(),
+                    Comment.findById(req.params.commentId).lean(),
+                ])
+                    .then(([reply, comment]) => {
+                        // ADD THE REPLY
+                        comment.comments.unshift(reply._id);
+
+                        return Promise.all([
+                            comment.save(),
+                        ]);
+                    })
+                    .then(() => {
+                        res.redirect('/posts/${req.params.postId}');
+                    })
+                    .catch(console.error);
+                // SAVE THE CHANGE TO THE PARENT DOCUMENT
+                return post.save();
+            })
     });
 };
